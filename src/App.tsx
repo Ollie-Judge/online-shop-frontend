c26297c4112fe05a9d1268228e78127837f3cdf1
Oliver Judge <oliver.judge@guestline.com>
2022-11-04 16:19:06 +0000

    conected up a local api that returns helloworld via usestate

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [output, setOutput] = useState('Click this button to fetch the Hello World api');
+
+  const handleClick = () => {
+    fetch("https://localhost:44361/HelloWorld", {
+      headers: {
+        XApiKey: "pgH7QzFHJx4w46fI~5Uzi4RvtTwlEXp"
+      }
+    
+    })
+    .then(response => response.text())
+    .then(output => setOutput(output))
+  }
+
   return (
     <div className="App">
-      <p>
-        Hello World
-      </p>
+      <h1>
+        Welcome to my Project
+      </h1>
+
+      <p>{output}</p>
+        <button className="ApiButton" onClick={handleClick}>Click this button to see an API being fetched using use state</button>
+
     </div>
   );
 }
